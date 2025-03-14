7fd72e7cfdd1ef66782f042612e63e98a5868b70
Priti Gupta <civilstudent9900@gmail.com>
2025-03-14 19:05:28 +0530

    persisting store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,19 @@ import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from './components/ui/sonner'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistor = persistStore(store);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <BrowserRouter>
   <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
     <App />
-  </Provider>
     <Toaster/>
+    </PersistGate>
+  </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
