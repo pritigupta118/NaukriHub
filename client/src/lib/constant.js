5cdf4a8343fa2d8647e5b02e91989182d34718fb
Priti Gupta <civilstudent9900@gmail.com>
2025-07-08 21:38:52 +0530

    Update API endpoints to point to the production server

--- client/src/lib/constant.js
+++ client/src/lib/constant.js
@@ -5,9 +5,9 @@
 // export const COMPANY_API_END_POINT = "https://naukrihub.onrender.com/company";
 // export const APPLICANTS_API_END_POINT = "https://naukrihub.onrender.com/application"
 
-export const USER_API_END_POINT = "http://localhost:3000/user";
+export const USER_API_END_POINT = "https://naukri-hub-server.vercel.app/user";
 
-export const JOB_API_END_POINT = "http://localhost:3000/job";
-export const APPLICATION_API_END_POINT = "http://localhost:3000/application";
-export const COMPANY_API_END_POINT = "http://localhost:3000/company";
-export const APPLICANTS_API_END_POINT = "http://localhost:3000/application"
+export const JOB_API_END_POINT = "https://naukri-hub-server.vercel.app/job";
+export const APPLICATION_API_END_POINT = "https://naukri-hub-server.vercel.app/application";
+export const COMPANY_API_END_POINT = "https://naukri-hub-server.vercel.app/company";
+export const APPLICANTS_API_END_POINT = "https://naukri-hub-server.vercel.app/application"
