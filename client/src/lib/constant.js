7135633e57204533aa2d261274753b299b70cccf
Priti Gupta <civilstudent9900@gmail.com>
2025-07-08 21:34:34 +0530

    Add server configuration files with package.json and vercel.json
    - Created package.json for server with dependencies and scripts for development and production.
    - Added vercel.json for deployment configuration on Vercel platform.

--- client/src/lib/constant.js
+++ client/src/lib/constant.js
@@ -1,6 +1,13 @@
-export const USER_API_END_POINT = "https://naukrihub.onrender.com/user";
+// export const USER_API_END_POINT = "https://naukrihub.onrender.com/user";
 
-export const JOB_API_END_POINT = "https://naukrihub.onrender.com/job";
-export const APPLICATION_API_END_POINT = "https://naukrihub.onrender.com/application";
-export const COMPANY_API_END_POINT = "https://naukrihub.onrender.com/company";
-export const APPLICANTS_API_END_POINT = "https://naukrihub.onrender.com/application"
+// export const JOB_API_END_POINT = "https://naukrihub.onrender.com/job";
+// export const APPLICATION_API_END_POINT = "https://naukrihub.onrender.com/application";
+// export const COMPANY_API_END_POINT = "https://naukrihub.onrender.com/company";
+// export const APPLICANTS_API_END_POINT = "https://naukrihub.onrender.com/application"
+
+export const USER_API_END_POINT = "http://localhost:3000/user";
+
+export const JOB_API_END_POINT = "http://localhost:3000/job";
+export const APPLICATION_API_END_POINT = "http://localhost:3000/application";
+export const COMPANY_API_END_POINT = "http://localhost:3000/company";
+export const APPLICANTS_API_END_POINT = "http://localhost:3000/application"
--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const _dirname = path.resolve()
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 const corsOption = {
-  origin: "https://naukrihub.onrender.com/",
+  origin: "http://localhost:5173",
   credentials: true
 }
 app.use(cors(corsOption))
